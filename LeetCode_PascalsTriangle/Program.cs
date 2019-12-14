e75a18f9ec780460d2a19ce53f0200bda27a1f07
John Weisz <johnmweisz@gmail.com>
2019-12-13 16:55:21 -0800

    Add project files.

---
+++ LeetCode_PascalsTriangle/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_PascalsTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            var answer = solution.Generate(4);
+            foreach (var row in answer)
+            {
+                foreach (var i in row)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    public class Solution
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            var result = new int[numRows][];
+            if (numRows < 1) return result;
+
+            result[0] = new int[1] { 1 };
+            if (numRows < 2) return result;
+
+            for (var i = 1; i < numRows; i++)
+            {
+                var row = new int[i + 1];
+                row[0] = result[i - 1][0];
+                for (var j = 1; j < row.Length - 1; j++)
+                {
+                    row[j] = result[i - 1][j - 1] + result[i - 1][j];
+                }
+                row[row.Length - 1] = result[i - 1][result[i - 1].Length - 1];
+                result[i] = row;
+            }
+
+            return result;
+        }
+    }
+
+}
